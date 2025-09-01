085eda4026bb288c12e4bc1e5787428683ed7784
Sirio Balmelli <sirio@b-ad.ch>
2025-09-01 15:58:04 +0200

    nix: reformat
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- flake.nix
+++ flake.nix
@@ -11,43 +11,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }: let
-    inherit (flake-utils.lib) eachDefaultSystem;
-  in
+  outputs =
     {
-      nixosModules.default = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        cfg = config.packages.flakem;
-        inherit (lib) mkEnableOption mkIf;
-      in {
-        options.packages.flakem.enable = mkEnableOption "flakem tools";
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      inherit (flake-utils.lib) eachDefaultSystem;
+    in
+    {
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.packages.flakem;
+          inherit (lib) mkEnableOption mkIf;
+        in
+        {
+          options.packages.flakem.enable = mkEnableOption "flakem tools";
 
-        config = mkIf cfg.enable {
-          nixpkgs.overlays = [self.overlays.default];
-          environment.systemPackages = with pkgs; [
-            build
-            build-there
-            ssh-wait
-            switch
-            switch-pull
-            switch-pull-reset
-            switch-push
-            switch-push-reset
-          ];
+          config = mkIf cfg.enable {
+            nixpkgs.overlays = [ self.overlays.default ];
+            environment.systemPackages = with pkgs; [
+              build
+              build-there
+              ssh-wait
+              switch
+              switch-pull
+              switch-pull-reset
+              switch-push
+              switch-push-reset
+            ];
+          };
         };
-      };
 
       overlays.default = final: prev: {
-        inherit
-          (prev.callPackage ./package.nix {inherit (prev.lib) makeScope;})
+        inherit (prev.callPackage ./package.nix { inherit (prev.lib) makeScope; })
           build
           build-there
           ssh-wait
@@ -60,13 +64,24 @@
       };
     }
     // eachDefaultSystem (system: {
-      packages = let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [self.overlays.default];
+      packages =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          inherit (pkgs)
+            build
+            build-there
+            ssh-wait
+            switch
+            switch-pull
+            switch-pull-reset
+            switch-push
+            switch-push-reset
+            ;
         };
-      in {
-        inherit (pkgs) build build-there ssh-wait switch switch-pull switch-pull-reset switch-push switch-push-reset;
-      };
     });
 }
--- package.nix
+++ package.nix
@@ -7,14 +7,15 @@
 
   nixos-rebuild,
   openssh,
-}: let
-  rebuildOpts = lib.cli.toGNUCommandLineShell {} {
+}:
+let
+  rebuildOpts = lib.cli.toGNUCommandLineShell { } {
     fast = true;
     use-remote-sudo = true;
     use-substitutes = true;
   };
 
-  nixOpts = lib.cli.toGNUCommandLineShell {} {
+  nixOpts = lib.cli.toGNUCommandLineShell { } {
     max-jobs = "auto";
     cores = 0;
   };
@@ -22,7 +23,7 @@
   shellApp = name: commandLine: {
     ${name} = writeShellApplication {
       inherit name;
-      runtimeInputs = [nixos-rebuild];
+      runtimeInputs = [ nixos-rebuild ];
       text = ''
         die() {
           echo "$*" >&2
@@ -60,7 +61,8 @@
     };
   };
 in
-  makeScope newScope (self:
+makeScope newScope (
+  self:
   ##
   # All scripts take:
   #
@@ -70,78 +72,86 @@ in
   # ${@:2}  : nix options
   #           options passed to nix as-is
   ##
-    lib.concatMapAttrs shellApp {
-      # build machine locally
-      # ... remember `'$` escape oddity
-      "build" = ''
-        nixos-rebuild build \
-          ${rebuildOpts} --flake ".#$FLAKE_TARGET" \
-          ${nixOpts} "''${NIX_OPTIONS[@]}"
-      '';
+  lib.concatMapAttrs shellApp {
+    # build machine locally
+    # ... remember `'$` escape oddity
+    "build" = ''
+      nixos-rebuild build \
+        ${rebuildOpts} --flake ".#$FLAKE_TARGET" \
+        ${nixOpts} "''${NIX_OPTIONS[@]}"
+    '';
 
-      # build machine remotely
-      "build-there" = ''
-        nixos-rebuild build \
-          ${rebuildOpts} --build-host "$DEPLOY_HOST" --target-host "$DEPLOY_HOST" \
-          --flake ".#$FLAKE_TARGET" \
-          ${nixOpts} "''${NIX_OPTIONS[@]}"
-      '';
+    # build machine remotely
+    "build-there" = ''
+      nixos-rebuild build \
+        ${rebuildOpts} --build-host "$DEPLOY_HOST" --target-host "$DEPLOY_HOST" \
+        --flake ".#$FLAKE_TARGET" \
+        ${nixOpts} "''${NIX_OPTIONS[@]}"
+    '';
 
-      # build machine locally, apply locally
-      "switch" = ''
-        nixos-rebuild switch \
-          ${rebuildOpts} --flake ".#$FLAKE_TARGET" \
-          ${nixOpts} "''${NIX_OPTIONS[@]}"
-      '';
+    # build machine locally, apply locally
+    "switch" = ''
+      nixos-rebuild switch \
+        ${rebuildOpts} --flake ".#$FLAKE_TARGET" \
+        ${nixOpts} "''${NIX_OPTIONS[@]}"
+    '';
 
-      # build machine remotely, apply remotely
-      "switch-pull" = ''
-        nixos-rebuild switch \
-          ${rebuildOpts} --build-host "$DEPLOY_HOST" --target-host "$DEPLOY_HOST" \
-          --flake ".#$FLAKE_TARGET" \
-          ${nixOpts} "''${NIX_OPTIONS[@]}"
-      '';
+    # build machine remotely, apply remotely
+    "switch-pull" = ''
+      nixos-rebuild switch \
+        ${rebuildOpts} --build-host "$DEPLOY_HOST" --target-host "$DEPLOY_HOST" \
+        --flake ".#$FLAKE_TARGET" \
+        ${nixOpts} "''${NIX_OPTIONS[@]}"
+    '';
 
-      # build machine locally, apply remotely
-      "switch-push" = ''
-        nixos-rebuild switch \
-          ${rebuildOpts} --target-host "$DEPLOY_HOST" --flake ".#$FLAKE_TARGET" \
-          ${nixOpts} "''${NIX_OPTIONS[@]}"
+    # build machine locally, apply remotely
+    "switch-push" = ''
+      nixos-rebuild switch \
+        ${rebuildOpts} --target-host "$DEPLOY_HOST" --flake ".#$FLAKE_TARGET" \
+        ${nixOpts} "''${NIX_OPTIONS[@]}"
+    '';
+  }
+  // {
+    # timeout-loop waiting for successful ssh
+    ssh-wait = writeShellApplication {
+      name = "ssh-wait";
+      runtimeInputs = [ openssh ];
+      text = ''
+        while ! ssh -o ConnectTimeout=5 -o ServerAliveInterval=5 "$@"; do
+          echo "$(date) ssh-wait: $*"
+        done
       '';
-    }
-    // {
-      # timeout-loop waiting for successful ssh
-      ssh-wait = writeShellApplication {
-        name = "ssh-wait";
-        runtimeInputs = [openssh];
-        text = ''
-          while ! ssh -o ConnectTimeout=5 -o ServerAliveInterval=5 "$@"; do
-            echo "$(date) ssh-wait: $*"
-          done
-        '';
-      };
+    };
 
-      # switch-pull, followed by a reboot and a nix-collect-garbage
-      switch-pull-reset = writeShellApplication {
-        name = "switch-pull-reset";
-        runtimeInputs = with self; [ssh-wait switch-pull];
-        text = /*bash*/ ''
+    # switch-pull, followed by a reboot and a nix-collect-garbage
+    switch-pull-reset = writeShellApplication {
+      name = "switch-pull-reset";
+      runtimeInputs = with self; [
+        ssh-wait
+        switch-pull
+      ];
+      text = # bash
+        ''
           switch-pull "$@"
           ssh "$1" "sudo reboot && while echo \"\$(date): waiting for reboot\"; do sleep 1; done" || true
           sleep 1  # patience: sometimes machines will *still* allow reconnect
           ssh-wait "$1" "sudo nix-collect-garbage --delete-older-than 15d"
         '';
-      };
+    };
 
-      # switch-push, followed by a reboot and a nix-collect-garbage
-      switch-push-reset = writeShellApplication {
-        name = "switch-push-reset";
-        runtimeInputs = with self; [ssh-wait switch-push];
-        text = ''
-          switch-push "$@"
-          ssh "$1" "sudo reboot && while echo \"\$(date): waiting for reboot\"; do sleep 1; done" || true
-          sleep 1  # patience: sometimes machines will *still* allow reconnect
-          ssh-wait "$1" "sudo nix-collect-garbage --delete-older-than 15d"
-        '';
-      };
-    })
+    # switch-push, followed by a reboot and a nix-collect-garbage
+    switch-push-reset = writeShellApplication {
+      name = "switch-push-reset";
+      runtimeInputs = with self; [
+        ssh-wait
+        switch-push
+      ];
+      text = ''
+        switch-push "$@"
+        ssh "$1" "sudo reboot && while echo \"\$(date): waiting for reboot\"; do sleep 1; done" || true
+        sleep 1  # patience: sometimes machines will *still* allow reconnect
+        ssh-wait "$1" "sudo nix-collect-garbage --delete-older-than 15d"
+      '';
+    };
+  }
+)
