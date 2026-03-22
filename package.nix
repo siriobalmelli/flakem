e63c156a971aeebbce082e103a7a6567c0f8013c
Sirio Balmelli <sirio@b-ad.ch>
2026-03-22 12:48:35 +0100

    switch: update system profile before activating on Darwin
    darwin-rebuild switch updates /nix/var/nix/profiles/system before
    calling activate. The flakem Darwin paths were only calling activate,
    leaving the profile pointing at the previous generation. A reboot would
    then silently revert to that stale generation.
    
    Add `sudo nix-env -p /nix/var/nix/profiles/system --set "$OUT_PATH"`
    before every Darwin activate call in switch, switch-pull, and
    switch-push.
    
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- package.nix
+++ package.nix
@@ -120,6 +120,7 @@ makeScope newScope (
           OUT_PATH=$(nix build --no-link --print-out-paths \
             ".#darwinConfigurations.$FLAKE_TARGET.system" \
             "''${NIX_OPTIONS[@]}")
+          sudo nix-env -p /nix/var/nix/profiles/system --set "$OUT_PATH"
           sudo "$OUT_PATH/activate"
         else
           nixos-rebuild switch ${rebuildOpts} --flake ".#$FLAKE_TARGET" "''${NIX_OPTIONS[@]}"
@@ -137,7 +138,7 @@ makeScope newScope (
           for opt in "''${NIX_OPTIONS[@]}"; do
             CMD="$CMD $(printf '%q' "$opt")"
           done
-          CMD="$CMD) && sudo \"\$OUT_PATH/activate\""
+          CMD="$CMD) && sudo nix-env -p /nix/var/nix/profiles/system --set \"\$OUT_PATH\" && sudo \"\$OUT_PATH/activate\""
           # shellcheck disable=SC2029
           ssh -t "$DEPLOY_HOST" "$CMD"
         elif [ "$(nix eval --json ".#nixosConfigurations" --apply "x: x ? \"$FLAKE_TARGET\"")" = "true" ]; then
@@ -160,7 +161,7 @@ makeScope newScope (
             "''${NIX_OPTIONS[@]}")
           nix copy --to "ssh://$DEPLOY_HOST" "$OUT_PATH"
           # shellcheck disable=SC2029
-          ssh -t "$DEPLOY_HOST" "sudo $OUT_PATH/activate"
+          ssh -t "$DEPLOY_HOST" "sudo nix-env -p /nix/var/nix/profiles/system --set $OUT_PATH && sudo $OUT_PATH/activate"
         elif [ "$(nix eval --json ".#nixosConfigurations" --apply "x: x ? \"$FLAKE_TARGET\"")" = "true" ]; then
           nixos-rebuild switch \
             ${rebuildOpts} --target-host "$DEPLOY_HOST" --flake ".#$FLAKE_TARGET" "''${NIX_OPTIONS[@]}"
